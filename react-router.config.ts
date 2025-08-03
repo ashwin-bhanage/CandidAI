a89f4aac1a680be1bd19ab47c38871ff9e3f23ff
Ashwin <bhanageashwin28@gmail.com>
2025-08-04 00:51:49 +0530

    Deploy initial

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false, //used for deploy on app entry point is true doesnt render.
 } satisfies Config;
