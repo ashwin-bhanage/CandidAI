8957b1fb97672dca6b6ff31f9c4f02b4da07ad29
Ashwin Bhanage <153538156+ashwin-bhanage@users.noreply.github.com>
2025-10-15 17:05:37 +0000

    Google Auth Console Search

--- app/root.tsx
+++ app/root.tsx
@@ -38,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="t58f-_jZ0Tz1-Q9wXHcGqM4lrai-5CP6HOM8akQ11h4" />
         <Meta />
         <Links />
       </head>
